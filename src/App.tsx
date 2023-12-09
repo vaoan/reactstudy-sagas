8cab45f8a82cca2f2ad10aee9a5cabb204712d80
Heiner Angarita <heinerangarita@gmail.com>
2023-12-09 10:20:27 -0500

    Adding i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const [t] = useTranslation(["general"]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {t("Hello")}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { I18nextProvider } from "react-i18next";
+import i18n from "configs/i18n";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
